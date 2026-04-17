3c2c8a5efc0fb24869a11ee0961ead9443455d30
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2026-04-18 01:55:37 +0500

    ListBox DONE

---
+++ Dialogs/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using WinForms = System.Windows.Forms;
+
+namespace Dialogs
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btnMessageBox_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("This is MessageBox", "Caption", MessageBoxButton.OK, MessageBoxImage.Information);
+		}
+
+		private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			if (dialog.ShowDialog() != DialogResult.HasValue)
+			{
+				MessageBox.Show(dialog.FileName, "Filename", MessageBoxButton.OK, MessageBoxImage.Information);
+			}
+		}
+
+		private void btnOpenFolder_Click(object sender, RoutedEventArgs e)
+		{
+			WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog();
+			dialog.ShowDialog();
+			MessageBox.Show(dialog.SelectedPath, "Folder", MessageBoxButton.OK, MessageBoxImage.Information);
+		}
+	}
+}
